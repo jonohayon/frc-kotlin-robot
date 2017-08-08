00b8b35a1b628a34f546bf84a7dafd1467398418
sJonathan Ohayon <me@jonathano.com>
2017-08-09 01:09:28 +0300

    🚀  Initial Commit

---
+++ src/org/usfirst/frc/team3316/robot/commands/ExampleCommand.kt
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team3316.robot.commands
+
+import edu.wpi.first.wpilibj.command.Command
+
+import org.usfirst.frc.team3316.robot.Robot
+
+/**
+ *
+ */
+class ExampleCommand: Command() {
+  init {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.exampleSubsystem)
+  }
+
+  // Called just before this Command runs the first time
+  override fun initialize () {
+    TODO("Not Implemented")
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  override fun execute () {
+    TODO("Not Implemented")
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  override fun isFinished (): Boolean {
+    return false
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  override fun end () {
+    TODO("Not Implemented")
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  override fun interrupted () {
+    TODO("Not Implemented")
+  }
+}
---
+++ src/org/usfirst/frc/team3316/robot/subsystems/ExampleSubsystem.kt
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team3316.robot.subsystems
+
+import edu.wpi.first.wpilibj.command.Subsystem
+
+/**
+ *
+ */
+class ExampleSubsystem: Subsystem() {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  override fun initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
